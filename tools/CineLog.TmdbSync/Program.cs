9aef014abd2b4e1c0333319423e4982142a17a19
radob <radob@cinelog.dev>
2026-03-30 10:08:47 +0200

    add tmdb-sync: worker service project setup

---
+++ tools/CineLog.TmdbSync/Program.cs
@@ -0,0 +1,77 @@
+using CineLog.TmdbSync.Data;
+using CineLog.TmdbSync.Infrastructure;
+using CineLog.TmdbSync.Sync;
+using DM.MovieApi;
+using DM.MovieApi.MovieDb.Discover;
+using DM.MovieApi.MovieDb.Genres;
+using DM.MovieApi.MovieDb.Movies;
+using DM.MovieApi.MovieDb.People;
+using DM.MovieApi.MovieDb.TV;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = Host.CreateApplicationBuilder(args);
+
+    builder.Services.AddSerilog((_, config) => config
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console());
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("DefaultConnection is not configured.");
+
+    var bearerToken = builder.Configuration["Tmdb:BearerToken"]
+        ?? throw new InvalidOperationException("Tmdb:BearerToken is not configured.");
+
+    // TMDb API clients
+    MovieDbFactory.RegisterSettings(bearerToken);
+    builder.Services.AddSingleton(_ => MovieDbFactory.Create<IApiMovieRequest>().Value);
+    builder.Services.AddSingleton(_ => MovieDbFactory.Create<IApiTVShowRequest>().Value);
+    builder.Services.AddSingleton(_ => MovieDbFactory.Create<IApiGenreRequest>().Value);
+    builder.Services.AddSingleton(_ => MovieDbFactory.Create<IApiPeopleRequest>().Value);
+    builder.Services.AddSingleton(_ => MovieDbFactory.Create<IApiDiscoverRequest>().Value);
+    builder.Services.AddSingleton(new TmdbDirectClient(bearerToken));
+
+    // Database contexts
+    builder.Services.AddDbContext<TmdbSyncDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    builder.Services.AddDbContext<TmdbSchemaDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    // Infrastructure
+    builder.Services.AddSingleton<TmdbRateLimiter>();
+    builder.Services.AddScoped<CheckpointService>();
+    builder.Services.AddScoped<FailureTracker>();
+
+    // Sync services
+    builder.Services.AddScoped<GenreSync>();
+    builder.Services.AddScoped<MovieFullSync>();
+    builder.Services.AddScoped<TvSeriesFullSync>();
+    builder.Services.AddScoped<PersonSync>();
+    builder.Services.AddHostedService<SyncWorker>();
+
+    var host = builder.Build();
+
+    // Ensure tmdb schema tables exist
+    using (var scope = host.Services.CreateScope())
+    {
+        var tmdbCtx = scope.ServiceProvider.GetRequiredService<TmdbSchemaDbContext>();
+        await tmdbCtx.Database.EnsureCreatedAsync();
+    }
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
