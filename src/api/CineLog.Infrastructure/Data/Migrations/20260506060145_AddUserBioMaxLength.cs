ee90cd8104f615305097f0faa599691456baa8b3
Adam Haluska <adam.haluska3@gmail.com>
2026-05-06 08:07:02 +0200

    fix missing DB migration

---
+++ src/api/CineLog.Infrastructure/Data/Migrations/20260506060145_AddUserBioMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineLog.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserBioMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "users",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
