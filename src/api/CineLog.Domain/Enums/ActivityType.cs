ac3b65e7bc05c2bf9704e134b399b69e33354010
Vladimír Borek <xborek@fi.muni.cz>
2026-05-23 12:34:17 +0200

    Expand activity log event types

--- src/api/CineLog.Domain/Enums/ActivityType.cs
+++ src/api/CineLog.Domain/Enums/ActivityType.cs
@@ -1,17 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+namespace CineLog.Domain.Enums;
 
-namespace CineLog.Domain.Enums
+public enum ActivityType
 {
-    public enum ActivityType
-    {
-        MovieWatched = 1,
-        MovieWatchLaterAdded = 2,
-        MovieFavorited = 3,
-        MovieAddedToCustomWatchlist = 4,
-        ReviewCreated = 5,
-        ReviewLiked = 6,
-        UserFollowed = 7
-    }
+    MovieWatched = 1,
+    MovieWatchLaterAdded = 2,
+    MovieFavorited = 3,
+    MovieAddedToCustomWatchlist = 4,
+    ReviewCreated = 5,
+    ReviewLiked = 6,
+    UserFollowed = 7,
+
+    MovieFavoriteRemoved = 8,
+    ReviewUnliked = 9,
+    UserUnfollowed = 10,
+    ProfileUpdated = 11,
+    AvatarUpdated = 12,
+    ReviewUpdated = 13,
+    ReviewDeleted = 14
 }
