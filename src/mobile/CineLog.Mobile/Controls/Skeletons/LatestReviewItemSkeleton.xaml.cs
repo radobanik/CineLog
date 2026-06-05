5b37b309ed5c7fb5afa7789ef54aed064a4379a1
radob <radob@cinelog.dev>
2026-06-05 21:41:27 +0200

    Add latest review skeletons

---
+++ src/mobile/CineLog.Mobile/Controls/Skeletons/LatestReviewItemSkeleton.xaml.cs
@@ -0,0 +1,9 @@
+namespace CineLog.Mobile.Controls;
+
+public partial class LatestReviewItemSkeleton : ContentView
+{
+    public LatestReviewItemSkeleton()
+    {
+        InitializeComponent();
+    }
+}
