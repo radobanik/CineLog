5c78a18784670dd6d04b7f75185a570a2994b7fc
Vladimír Borek <xborek@fi.muni.cz>
2026-06-07 23:53:51 +0200

    remove stars from watchlist movie and link watchlist movie to movie detail

--- src/mobile/CineLog.Mobile/View/WatchList/WatchListMovieRowView.xaml.cs
+++ src/mobile/CineLog.Mobile/View/WatchList/WatchListMovieRowView.xaml.cs
@@ -4,8 +4,17 @@ namespace CineLog.Mobile.Views.WatchList;
 
 public partial class WatchListMovieRowView : ContentView
 {
+    public static readonly BindableProperty OpenCommandProperty =
+        BindableProperty.Create(nameof(OpenCommand), typeof(ICommand), typeof(WatchListMovieRowView));
+
     public static readonly BindableProperty RemoveCommandProperty =
         BindableProperty.Create(nameof(RemoveCommand), typeof(ICommand), typeof(WatchListMovieRowView));
+
+    public ICommand? OpenCommand
+    {
+        get => (ICommand?)GetValue(OpenCommandProperty);
+        set => SetValue(OpenCommandProperty, value);
+    }
 
     public ICommand? RemoveCommand
     {
